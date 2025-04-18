e4d2c57ce48de339c7ceaa6a8a9ca7a9be09a946
Chandan R <chandan55.smg@gmail.com>
2025-04-18 19:39:43 +0530

    Final app done

--- frontend/src/store/useProductStore.jsx
+++ frontend/src/store/useProductStore.jsx
@@ -65,7 +65,37 @@ export const useProductStore = create((set, get) => ({
             set({ error: error.message, loading: false });
             toast.error("Error deleting product")
         }
+    },
+
+    fetchProduct: async (id) => {
+        set({ loading: true });
+        try {
+            const response = await axios.get(`${BASE_URL}/api/products/${id}`);
+            set({ product: response.data.data, formData: response.data.data, loading: false });
+        } catch (error) {
+            if (error.status == 429) {
+                set({ error: "Too many requests, please try again later", products: [], loading: false });
+            }
+            else {
+                set({ error: error.message, loading: false });
+            }
+
+        }
+    },
+
+    updateProduct: async (id, formData) => {
+        set({ loading: true });
+        try {
+            const response = await axios.put(`${BASE_URL}/api/products/${id}`, formData);
+            set((state) => ({
+                products: state.products.map((product) => product.id === id ? response.data.data : product),
+                loading: false,
+            }));
+            toast.success("Product updated successfully");
+        } catch (error) {
+            set({ error: error.message, loading: false });
+            toast.error("Error updating product");
+
+        }
     }
-
-
 }));
